2530e0c637cbd201798432ee605ae0ecd45897c3
brian.lemur.ackley <brian.lemur.ackley@gmail.com>
2020-12-30 07:51:44 -0800

    revert to basic, original implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,14 @@
 export default {
-  mode: 'spa',
+ /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
   /*
    ** Headers of the page
    */
@@ -25,13 +34,38 @@ export default {
    */
   css: [],
   /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+    '~/plugins/components',
+    '~/plugins/composition-api.js',
+    '~/plugins/storyblok-rich-text-renderer.js'
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: [
+    [
+      'storyblok-nuxt',
+      {
+        accessToken: "1nrsQ51JknNvrt2l6yGfIwtt",
+        cacheProvider: 'memory'
+      }
+    ],
+  ],
   /*
    ** Build configuration
    */
